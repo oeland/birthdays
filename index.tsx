3d08a5aaa4e6dc364d8ee7ea509768e0dcf8c9ab
oeland <myrkin@gmail.com>
2022-03-30 18:04:49 +0200

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import {
+  format,
+  formatDistance,
+  formatRelative,
+  subDays,
+  differenceInDays,
+} from 'date-fns';
 
 type AppProps = {
   persons: {
     name: string;
     birthday: Date;
   }[];
-}
+};
 
 class App extends Component<AppProps> {
   constructor(props) {
@@ -26,43 +33,72 @@ class App extends Component<AppProps> {
   }
 }
 
-function GetThisYearsDate(props: { name?: string; birthday: Date; })
-{
+function GetThisYearsBirthdate(props: { name?: string; birthday: Date }) {
   var now = new Date();
   const dob = props.birthday;
 
-  return (<td>
-    {dob.toString('dd/MM')}/{now.getFullYear()}
-    </td>);
+  return format(dob, 'dd/MM') + '/' + now.getFullYear();
+}
+
+function GetDaysUntilBirthday(props: { name?: string; birthday: Date }) {
+  var now = new Date();
+  var birthday = new Date(
+    now.getFullYear(),
+    props.birthday.getMonth(),
+    props.birthday.getDate()
+  );
+  var birthdayNextYear = new Date(
+    now.getFullYear() + 1,
+    props.birthday.getMonth(),
+    props.birthday.getDate()
+  );
+
+  var rawDifference = differenceInDays(birthday, now);
+  var difference = 0;
+  if (rawDifference > -1) {
+    difference = rawDifference;
+  } else {
+    difference = differenceInDays(birthdayNextYear, now);
+  }
+
+  console.log(birthday, birthdayNextYear, difference, rawDifference);
+
+  return difference;
 }
 
 class PersonList extends Component<AppProps> {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
-        return (
-          <table>
-            {this.props.persons.map((person) => 
-              <tr>
-                <td>{person.name}</td>
-                <td>{GetThisYearsDate(person)}</td>
-              </tr>
-            )}
-          </table>
-        );
-  };
+    return (
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Born</th>
+          <th>Days to birthday</th>
+        </tr>
+        {this.props.persons.map((person) => (
+          <tr>
+            <td>{person.name}</td>
+            <td>{GetThisYearsBirthdate(person)}</td>
+            <td>{GetDaysUntilBirthday(person)}</td>
+          </tr>
+        ))}
+      </table>
+    );
+  }
 }
 
 const personList = [
-  { name: "Kristoffer Hansen", birthday: new Date("1980-09-03")},
-  { name: "Mikkel Hansen", birthday: new Date("1978-07-18")},
-  { name: "Storm Hansen", birthday: new Date("2010-08-02")},
-  { name: "Gunnar Hansen", birthday: new Date("1947-07-08")},
-  { name: "Kamma Hansen", birthday: new Date("1950-10-10")},
-  { name: "Ann-Mai Hansen", birthday: new Date("1983-11-07")},
-  { name: "Erling Hansen", birthday: new Date("1941-08-15")}
+  { name: 'Kristoffer Hansen', birthday: new Date('1980-09-03') },
+  { name: 'Mikkel Hansen', birthday: new Date('1978-07-18') },
+  { name: 'Storm Hansen', birthday: new Date('2010-08-02') },
+  { name: 'Gunnar Hansen', birthday: new Date('1947-07-08') },
+  { name: 'Kamma Hansen', birthday: new Date('1950-10-10') },
+  { name: 'Ann-Mai Hansen', birthday: new Date('1983-11-07') },
+  { name: 'Erling Hansen', birthday: new Date('1941-08-15') },
 ];
 
-render(<App persons={personList}/>, document.getElementById('root'));
+render(<App persons={personList} />, document.getElementById('root'));
