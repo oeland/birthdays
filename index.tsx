4647a432804c81e01bf44e4c0c409d1d6b178e9b
oeland <myrkin@gmail.com>
2022-03-30 23:21:28 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -8,9 +8,7 @@ type Person = {
   birthdate: Date;
 };
 
-type Persons = {
-  people: Person[];
-};
+type Persons = Person[];
 
 type AppProps = {
   family: Persons;
@@ -91,28 +89,24 @@ class PersonList extends Component<Persons> {
   }
 }
 
-const familyList: Persons = {
-  people: [
-    { name: 'Kristoffer Hansen', birthdate: new Date('1980-09-03') },
-    { name: 'Mikkel Hansen', birthdate: new Date('1978-07-18') },
-    { name: 'Storm Hansen', birthdate: new Date('2010-08-02') },
-    { name: 'Gunnar Hansen', birthdate: new Date('1947-07-08') },
-    { name: 'Kamma Hansen', birthdate: new Date('1950-10-10') },
-    { name: 'Ann-Mai Hansen', birthdate: new Date('1983-11-07') },
-    { name: 'Erling Hansen', birthdate: new Date('1941-08-15') },
-  ],
-};
+const familyList: Persons = [
+  { name: 'Kristoffer Hansen', birthdate: new Date('1980-09-03') },
+  { name: 'Mikkel Hansen', birthdate: new Date('1978-07-18') },
+  { name: 'Storm Hansen', birthdate: new Date('2010-08-02') },
+  { name: 'Gunnar Hansen', birthdate: new Date('1947-07-08') },
+  { name: 'Kamma Hansen', birthdate: new Date('1950-10-10') },
+  { name: 'Ann-Mai Hansen', birthdate: new Date('1983-11-07') },
+  { name: 'Erling Hansen', birthdate: new Date('1941-08-15') },
+];
 
-const friendsList: Persons = {
-  people: [
-    { name: 'Rene Christensen', birthdate: new Date('1980-09-03') },
-    { name: 'Mikkel Hansen', birthdate: new Date('1979-02-02') },
-    { name: 'Mark Herdal', birthdate: new Date('1978-03-20') },
-    { name: 'Lars Lauritzen', birthdate: new Date('1975-12-17') },
-    { name: 'Jonas Christensen', birthdate: new Date('1979-11-22') },
-    { name: 'Thomas Hansen', birthdate: new Date('1978-11-04') },
-  ],
-};
+const friendsList: Persons = [
+  { name: 'Rene Christensen', birthdate: new Date('1980-09-03') },
+  { name: 'Mikkel Hansen', birthdate: new Date('1979-02-02') },
+  { name: 'Mark Herdal', birthdate: new Date('1978-03-20') },
+  { name: 'Lars Lauritzen', birthdate: new Date('1975-12-17') },
+  { name: 'Jonas Christensen', birthdate: new Date('1979-11-22') },
+  { name: 'Thomas Hansen', birthdate: new Date('1978-11-04') },
+];
 
 render(
   <App family={familyList} friends={friendsList} />,
