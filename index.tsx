8f13ea16160f13a0479d298293836440f5610e7c
oeland <myrkin@gmail.com>
2022-03-30 23:47:53 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import { format, differenceInDays } from 'date-fns';
+import { format, differenceInDays, compareDesc } from 'date-fns';
 
 type Person = {
   name: string;
@@ -35,37 +35,27 @@ class App extends Component<AppProps> {
   }
 }
 
-function GetThisYearsBirthdate(props: Person) {
-  var now = new Date();
+function GetNextBirthday(props: Person) {
   const dob = props.birthdate;
+  const now = new Date();
+  const birthDayCurrentYear = new Date(
+    now.getFullYear(),
+    dob.getMonth(),
+    dob.getDate()
+  );
+  var addYear = 0;
+  if (compareDesc(birthDayCurrentYear, now) > 0) addYear++;
 
-  return format(dob, 'dd/MM') + '/' + now.getFullYear();
+  return new Date(now.getFullYear() + addYear, dob.getMonth(), dob.getDate());
 }
 
 function GetDaysUntilBirthday(props: Person) {
-  var now = new Date();
-  var birthday = new Date(
-    now.getFullYear(),
-    props.birthdate.getMonth(),
-    props.birthdate.getDate()
-  );
-  var birthdayNextYear = new Date(
-    now.getFullYear() + 1,
-    props.birthdate.getMonth(),
-    props.birthdate.getDate()
-  );
+  const now = new Date();
+  const nextBirthDay = GetNextBirthday(props);
 
-  var rawDifference = differenceInDays(birthday, now);
-  var difference = 0;
-  if (rawDifference > -1) {
-    difference = rawDifference;
-  } else {
-    difference = differenceInDays(birthdayNextYear, now);
-  }
+  console.log(now, nextBirthDay);
 
-  console.log(birthday, birthdayNextYear, difference, rawDifference);
-
-  return difference;
+  return differenceInDays(nextBirthDay, now) + 1;
 }
 
 class PersonList extends Component<Persons> {
@@ -80,7 +70,7 @@ class PersonList extends Component<Persons> {
         {this.props.people.map((p) => (
           <tr>
             <td>{p.name}</td>
-            <td>{GetThisYearsBirthdate(p)}</td>
+            <td>{format(GetNextBirthday(p), 'dd-MM-yyyy')}</td>
             <td>{GetDaysUntilBirthday(p)}</td>
           </tr>
         ))}
